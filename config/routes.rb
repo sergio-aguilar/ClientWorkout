04138395745c9bdfa06f0553596df76d142775fc
Sergio Aguilar <aguiserg@gmail.com>
2012-07-18 20:19:36 -0500

    Add form view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 HeadfirstClientworkout::Application.routes.draw do
   resources :client_workouts
+  
+  match 'client_workouts/find' => 'ClientWorkouts#find'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
