330193fecbe84f53241c61680ae5939f2b6d8170
Sergio Aguilar <aguiserg@gmail.com>
2012-07-19 17:52:17 -0500

    Validate code

--- app/models/client_workout.rb
+++ app/models/client_workout.rb
@@ -1,3 +1,6 @@
 class ClientWorkout < ActiveRecord::Base
   attr_accessible :client_name, :date_of_workout, :duration_mins, :paid_amount, :trainer
+  validates_numericality_of :paid_amount, :message => "is not a number"
+  validates_presence_of :trainer, :message => "can't be blank"
+  validates_presence_of :client_name, :message => "can't be blank"
 end
